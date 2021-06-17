65f42cfa925e9ff187643dc4eff897ae238c79a6
Federico Olive <FedericoOlive@gmail.com>
2021-06-17 17:41:02 -0300

    Add MainMenu

---
+++ TSDV_Desarrollo02_TP_11_2942/Assets/Scripts/Ui/UiButtonEffect.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class UiButtonEffect : MonoBehaviour
+{
+    [SerializeField] private float scaleMultiply = 3;
+    [SerializeField] private float limit = 1.2f;
+    private bool increment = false;
+    private Vector3 initialScale;
+    private Vector3 scale;
+
+    private void Awake()
+    {
+        increment = false;
+        initialScale = transform.localScale;
+    }
+
+    private void OnEnable()
+    {
+        transform.localScale = initialScale;
+        increment = false;
+    }
+
+    private void Update()
+    {
+        ChangeScale();
+    }
+
+    public void OnMouseEnterButton()
+    {
+        increment = true;
+    }
+    public void OnMouseExitButton()
+    {
+        increment = false;
+    }
+    private void ChangeScale()
+    {
+        float timeStep = scaleMultiply * Time.deltaTime;
+        scale = transform.localScale;
+        if (increment)
+        {
+            if (transform.localScale.x < limit)
+            {
+                scale = new Vector3(scale.x + timeStep, scale.y + timeStep, scale.z + timeStep);
+                transform.localScale = scale;
+            }
+            else
+            {
+                transform.localScale = new Vector3(limit, limit, limit);
+            }
+        }
+        else
+        {
+            if (transform.localScale.x > initialScale.x)
+            {
+                scale = new Vector3(scale.x - timeStep, scale.y - timeStep, scale.z - timeStep);
+                transform.localScale = scale;
+            }
+            else
+            {
+                transform.localScale = new Vector3(initialScale.x, initialScale.y, initialScale.z);
+            }
+        }
+    }
+}
---
+++ TSDV_Desarrollo02_TP_11_2942/Assets/Scripts/Ui/UiManager.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public bool debug;
+    private String[] menues = {"Main Menu", "Credits", "Exit"};
+
+    [Serializable]
+    public class PanelsList
+    {
+        public List<Image> images = new List<Image>();
+        public List<TextMeshProUGUI> texts = new List<TextMeshProUGUI>();
+    }
+
+    public List<PanelsList> menu = new List<PanelsList>();
+    public GameObject panelBlockRaycast;
+    bool buttonPressed = false;
+    int currentMenu = 0;
+    [SerializeField] private float durationTransition = 2;
+    private float colortime;
+
+    private void Start()
+    {
+        GameObject[] panels = GameObject.FindGameObjectsWithTag("Panels");
+
+        for (int i = 0; i < panels.Length; i++)
+        {
+            menu.Add(new PanelsList());
+
+            foreach (Image child in panels[i].GetComponentsInChildren<Image>(true))
+            {
+                menu[i].images.Add(child);
+                child.gameObject.SetActive(false);
+            }
+
+            foreach (TextMeshProUGUI child in panels[i].GetComponentsInChildren<TextMeshProUGUI>(true))
+            {
+                menu[i].texts.Add(child);
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        for (int i = 0; i < menu.Count; i++)
+        {
+            if (i == (int)currentMenu)
+            {
+                foreach (var btn in menu[i].images)
+                {
+                    btn.gameObject.SetActive(true);
+                }
+
+                foreach (var txt in menu[i].texts)
+                {
+                    txt.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (buttonPressed)
+        {
+            OnMenu();
+            panelBlockRaycast.SetActive(buttonPressed);
+        }
+    }
+
+    public void ButtonPressed(int _currentMenu)
+    {
+        if (debug) Debug.Log("Cambio a Panel: " + menues[_currentMenu]);
+        buttonPressed = true;
+        currentMenu = _currentMenu;
+
+        for (int i = 0; i < menu.Count; i++)
+        {
+            if (i == (int) currentMenu)
+            {
+                foreach (var btn in menu[i].images)
+                {
+                    btn.gameObject.SetActive(true);
+                    Color color = btn.color;
+                    btn.color = new Color(color.r, color.g, color.b, 0);
+                }
+
+                foreach (var txt in menu[i].texts)
+                {
+                    txt.gameObject.SetActive(true);
+                    Color color = txt.color;
+                    txt.color = new Color(color.r, color.g, color.b, 0);
+                }
+            }
+        }
+    }
+
+    private void OnMenu()
+    {
+        float deltaT = Time.deltaTime;
+        colortime += deltaT;
+
+        for (int i = 0; i < menu.Count; i++)
+        {
+            if (i != (int)currentMenu)
+            {
+                foreach (var btn in menu[i].images)
+                {
+                    Color color = btn.color;
+                    color.a = 1;
+                    Color colorTransparent = new Color(color.r, color.g, color.b, 0);
+                    btn.color = Color.Lerp(color, colorTransparent, colortime / durationTransition);
+
+                    if (colortime > durationTransition || !buttonPressed)
+                    {
+                        btn.color = new Color(color.r, color.g, color.b, 0);
+                        btn.gameObject.SetActive(false);
+                        buttonPressed = false;
+                        colortime = 0;
+                    }
+                }
+
+                foreach (var txt in menu[i].texts)
+                {
+                    Color color = txt.color;
+                    color.a = 1;
+                    Color colorTransparent = new Color(color.r, color.g, color.b, 0);
+
+                    txt.color = Color.Lerp(color, colorTransparent, colortime / durationTransition);
+
+                    if (colortime > durationTransition || !buttonPressed)
+                    {
+                        txt.color = new Color(color.r, color.g, color.b, 0);
+                        txt.gameObject.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var btn in menu[i].images)
+                {
+                    Color color = btn.color;
+                    color.a = 0;
+                    Color colorOpaque = new Color(color.r, color.g, color.b, 1);
+                    btn.color = Color.Lerp(color, colorOpaque, colortime);
+
+                    if (colortime > durationTransition || !buttonPressed)
+                    {
+                        btn.color = new Color(color.r, color.g, color.b, 1);
+                        buttonPressed = false;
+                    }
+                }
+
+                foreach (var txt in menu[i].texts)
+                {
+                    Color color = txt.color;
+                    color.a = 0;
+                    Color colorOpaque = new Color(color.r, color.g, color.b, 1);
+
+                    txt.color = Color.Lerp(color, colorOpaque, colortime / durationTransition);
+
+                    if (colortime > durationTransition || !buttonPressed)
+                    {
+                        txt.color = new Color(color.r, color.g, color.b, 1);
+                    }
+                }
+            }
+        }
+    }
+
+    public void ChangeScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
