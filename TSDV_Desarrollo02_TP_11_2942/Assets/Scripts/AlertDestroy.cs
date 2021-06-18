4efdcbe09fd19c9c844293e4474a40db911e8ce6
Federico Olive <FedericoOlive@gmail.com>
2021-06-18 10:56:33 -0300

    Add Ui assets.

--- TSDV_Desarrollo02_TP_11_2942/Assets/Scripts/AlertDestroy.cs
+++ TSDV_Desarrollo02_TP_11_2942/Assets/Scripts/AlertDestroy.cs
@@ -7,7 +7,7 @@ public class AlertDestroy : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other)
     {
-        Debug.LogError("Un objeto pasó el límite: " + other.gameObject.name + " Contra: " + gameObject.name);
+        Debug.LogWarning("Un objeto pasó el límite: " + other.gameObject.name + " Contra: " + gameObject.name);
         Destroy(other.gameObject);
     }
 }
