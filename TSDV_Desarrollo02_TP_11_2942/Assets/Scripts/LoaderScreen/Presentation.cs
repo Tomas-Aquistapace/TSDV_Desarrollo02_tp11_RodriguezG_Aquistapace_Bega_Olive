36377b362ad44123ede773e5d95ed5455f1bc536
Federico Olive <FedericoOlive@gmail.com>
2021-06-18 10:57:02 -0300

    Add audio and presentation scene.

---
+++ TSDV_Desarrollo02_TP_11_2942/Assets/Scripts/LoaderScreen/Presentation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Presentation : MonoBehaviour
+{
+    public RectTransform logo;
+
+    private float onTime;
+    public float maxTime;
+    public float initialScale;
+    public float maxScale;
+    void Start()
+    {
+        logo.localScale = new Vector3(initialScale, initialScale, initialScale);
+        Invoke(nameof(ChangeScene), maxTime);
+    }
+
+
+    void Update()
+    {
+        onTime += Time.deltaTime;
+        float actualScale = Mathf.Lerp(initialScale, maxScale, onTime / maxTime);
+        logo.localScale = new Vector3(actualScale, actualScale, actualScale);
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
